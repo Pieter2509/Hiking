319b57561a69e9ea9711f1a8645318289467a8b7
Pieter2509 <pieterhollanders2509@gmail.com>
2026-07-22 21:57:09 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,7 @@ const GITHUB_REPO = {
 // iemand hiermee kan is deze ene workflow nog eens starten.
 const REFRESH_CONFIG = {
   workerUrl: "https://hiking.pieterhollanders2509.workers.dev",
-  triggerSecret: "Hiking-Pieter-25092000",
+  triggerSecret: "test",
 };
 
 const fallbackLink = document.getElementById("refresh-fallback-link");
